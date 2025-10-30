a737c9993d9df0e8717017e4cead8e294f51b12e
KunalTanwar130 <kunaldropshipping13@gmail.com>
2025-10-30 14:33:19 +0530

    Create DBConnection.java

---
+++ DBConnection.java
@@ -0,0 +1,24 @@
+package com.login;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    private static Connection conn = null;
+
+    public static Connection getConnection() {
+        try {
+            if (conn == null) {
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/userdb",
+                    "root",
+                    "root"
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
